bd42786edf2485f3c1d2349b22cf105e65f81df4
manuel-heav <manuelheav32@gmail.com>
2022-12-11 10:38:12 +0300

    new

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,8 @@
-# Client configuration file with Puppet
-file_line {  'passw_no_autentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line {  'passw_direction':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
+# Configures SSH client to disable accepting password for authentication and
+# adds ~/.ssh/school to the  list of ssh keys to check when establishing
+# connection.
+exec {'modify_ssh_config':
+  command  => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config;
+   echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  provider => 'shell'
 }
